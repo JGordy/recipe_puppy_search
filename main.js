d41fc77cd46d98159553030f143e0c5009883581
Joseph Gordy <jgordy2424@gmail.com>
2017-08-08 15:48:29 -0400

    finished basic functionality

---
+++ main.js
@@ -0,0 +1,41 @@
+let container = document.getElementById("container");
+
+// fetch call
+fetch("https://recipepuppyproxy.herokuapp.com/api/?q=bacon")
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log("Response status: ", response);
+      return;
+    }
+    response.json().then(function(data) {
+      console.log("test", response.url);
+
+      console.log(data);
+
+      for (var i = 0; i < data.results.length; i++) {
+
+        let recipe = document.createElement("a");
+        let h2 = document.createElement("h2");
+        let thumbnail = document.createElement("img");
+
+        recipe.setAttribute("href", "${data.results[i].href}");
+        if (data.results[i].thumbnail === "") {
+          thumbnail.setAttribute("src", "http://via.placeholder.com/180x130")
+        } else {
+          thumbnail.setAttribute("src", `${data.results[i].thumbnail}`);
+        }
+
+
+        h2.textContent = `${data.results[i].title}`;
+
+        container.appendChild(recipe);
+        recipe.appendChild(thumbnail);
+        recipe.appendChild(h2);
+      }
+
+
+    });
+  })
+  .catch(function(err) {
+    console.log("Fetch Error :-S", err);
+  });
